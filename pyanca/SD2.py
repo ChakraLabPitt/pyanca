e4bbd72d4e32dde256f815d8d6cd938bbf5d351c
Akash Parvatikar <akp47@pitt.edu>
2018-06-23 18:10:04 -0400

    Update SD2.py

--- pyanca/SD2.py
+++ pyanca/SD2.py
@@ -1,16 +1,17 @@
-"""
 ##############################################################################
 
-pyANCA --- http://www.csb.pitt.edu/anca/
+# pyANCA --- http://www.csb.pitt.edu/anca/
 
-Copyright (c) 2017, University of Pittsburgh.
+# Copyright (c) 2017, University of Pittsburgh.
 
-Released under the BSD 3-Clause License
+# Released under the BSD 3-Clause License
 
-Author: Akash Parvatikar
+# Author: Akash Parvatikar
+# Contributors: Gabriel Vacaliuc, Arvind Ramanathan, Chakra Chennubhotla
 
-Contributors: Gabriel Vacaliuc, Arvind Ramanathan, Chakra Chennubhotla
+##############################################################################
 
+"""
 SD2 module - 
 
 This module contains only one function, SD2, which performs spatial decorrelation of 2nd order.
