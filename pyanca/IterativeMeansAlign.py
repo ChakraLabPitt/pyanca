7b0c15dfd956a16818c1269921a3099084141778
Akash Parvatikar <akp47@pitt.edu>
2018-06-23 23:04:51 -0400

    Update IterativeMeansAlign.py

--- pyanca/IterativeMeansAlign.py
+++ pyanca/IterativeMeansAlign.py
@@ -1,3 +1,20 @@
+##############################################################################
+
+# pyANCA --- http://www.csb.pitt.edu/anca/
+
+# Copyright (c) 2017, University of Pittsburgh
+# Released under the BSD 3-Clause License
+
+# Author: Arvind Ramanathan
+# Contributor: Chakra Chennubhotla
+
+# Please cite your use of pyANCA in published work:
+# Parvatikar, A., Vacaliuc, G. S., Ramanathan, A., & Chennubhotla, S. C. (2018). 
+# ANCA: Anharmonic Conformational Analysis of Biomolecular Simulations. 
+# Biophysical journal, 114(9), 2040-2043.
+
+##############################################################################
+
 import MDAnalysis
 import numpy
 import numpy.linalg
